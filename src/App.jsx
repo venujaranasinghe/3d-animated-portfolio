a1031aa0914fa152f4791fb669f8757b271989cf
venujaranasinghe <it23225510@my.sliit.lk>
2025-02-03 15:05:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className=''>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </div>
   )
 }
 
