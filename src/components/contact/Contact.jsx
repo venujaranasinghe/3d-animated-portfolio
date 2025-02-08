ac7a7fa05dba2131c534009e2402ee364a2f64b8
venujaranasinghe <it23225510@my.sliit.lk>
2025-02-08 14:00:41 +0530

    ccc

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,59 @@
 import "./contact.css"
+import emailjs from '@emailjs/browser';
+import {useRef, useState} from "react"
 
 const Contact = () => {
+
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm(
+            import.meta.env.VITE_SERVICE_ID,
+            import.meta.env.VITE_TEMPLATE_ID, 
+            form.current, {
+            publicKey: import.meta.env.VITE_PUBLIC_KEY,
+          })
+          .then(
+            () => {
+              setSuccess(true)
+            },
+            (error) => {
+              setError(true)
+            },
+          );
+      };
+
     return (
-        <div className="contact">
-           <div className="cSection left">
+        <div className="contact" ref={form} onSubmit={sendEmail}>
+           <div className="cSection">
             <form>
                 <h1 className="cTitle">
                     Let's Talk!
                 </h1>
                 <div className="formItem">
                     <label>Your Name</label>
-                    <input type="text" placeholder="Enter your name"/>
+                    <input type="text" name="user_username" placeholder="Enter your name"/>
                 </div>
                 <div className="formItem">
                     <label>Your Email</label>
-                    <input type="email" placeholder="Enter your email"/>
+                    <input type="email" name="user_email" placeholder="Enter your email"/>
                 </div>
                 <div className="formItem">
                     <label>Write Your Message Here</label>
-                    <textarea rows="10" placeholder="Enter your message"></textarea>
+                    <textarea rows={10} name="user_message" placeholder="Enter your message"></textarea>
                 </div>
                 <button className="formButton">Submit</button>
+                {success && <span>Your message has been sent!</span>}
+                {error && <span>Something went wrong!</span>}
+                
             </form>
            </div>
-           <div className="cSection right">
-            
+           <div className="cSection">
+            SVG
            </div>
         </div>
     )
